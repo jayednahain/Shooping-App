00bb2fec08ce4325fc814f23421efdbe9f893a93
jayednahian <jayednahian@yahoo.com>
2023-02-04 13:25:59 +0600

    189-seperate single product complete

--- lib/widgets/productItem.dart
+++ lib/widgets/productItem.dart
@@ -2,25 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ProductItem extends StatelessWidget {
-
   final String? id;
   final String? title;
   final String? imageUrl;
 
-  ProductItem({
-    super.key,
-    this.id,
-    this.title,
-    this.imageUrl
-    });
+  ProductItem({super.key, this.id, this.title, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child:Image.network(
-        imageUrl!,
-        //image will take all the abatable space
-        fit: BoxFit.cover),
+      header: Text("hellow"),
+      child: Image.network(imageUrl!, fit: BoxFit.cover),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(title!, textAlign: TextAlign.center),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_bag),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
